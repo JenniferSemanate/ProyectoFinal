9b8bd21b2173a6118f1223bfe017bc4a1e2d3160
tereh <terehdz.th@gmail.com>
2022-04-10 21:56:02 +0200

    MealType.

--- src/views/Menus.vue
+++ src/views/Menus.vue
@@ -4,7 +4,7 @@
       <div v-if="loading">Loading....</div>
       <div v-if="data">
         <h1 class="p-5 bg-blue-50 font-semibold">Opciones de Desayuno!</h1>
-        <div class="grid md:grid-cols-4 lg:grid-cols-10 bg-blue-50 text-gray-600">
+        <div class="grid md:grid-cols-4 lg:grid-cols-6 bg-blue-50 text-gray-600">
           <div v-for="i in data.hits" :key="i" class="col-span-2 m-5">
             <div class="bg-white shadow-md p-8 rounded-xl text-center hover:shadow-2xl py-9">
               <p class="mb-5 font-bold text-lg">
