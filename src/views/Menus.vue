ad62cc368586dedb8d4b2cb62d42c859fccc7705
Jennifer Semanate <jennifersemanate@MacBook-Air-de-Jennifer.local>
2022-04-10 20:58:39 +0200

    enus

--- src/views/Menus.vue
+++ src/views/Menus.vue
@@ -3,16 +3,26 @@
     <div>
       <div v-if="loading">Loading....</div>
       <div v-if="data">
-        <h1 class="p-5  bg-blue-50 font-semibold">Opciones de Desayuno!</h1>
-        <div class="grid md:grid-cols-4 lg:grid-cols-10 bg-blue-50 text-gray-600">
-          <div v-for="menu in data" :key="menu" class="col-span-2 m-5" >
-            <div class="bg-white shadow-md p-8 rounded-xl text-center hover:shadow-2xl py-9">
-              <p class="mb-5 font-bold text-lg">{{ data.hits[2].recipe.label }}</p>
-              <img :src="data.hits[2].recipe.image" class="w-full h-64 mb-5">
-              <p class="mb-5">Calorias: {{ data.hits[1].recipe.calories }}</p>
+        <h1 class="p-5 bg-blue-50 font-semibold">Opciones de Desayuno!</h1>
+        <div
+          class="grid md:grid-cols-4 lg:grid-cols-10 bg-blue-50 text-gray-600"
+        >
+          <div v-for="i in data.hits" :key="i" class="col-span-2 m-5">
+            <div
+              class="bg-white shadow-md p-8 rounded-xl text-center hover:shadow-2xl py-9"
+            >
+              <p class="mb-5 font-bold text-lg">
+                {{ [i] }}
+              </p>
+              <!-- <img :src="data.hits[2].recipe.image" class="w-full h-64 mb-5" /> -->
+              <!-- <p class="mb-5">Calorias: {{ data.hits[1].recipe.calories }}</p> -->
               <div class="flex justify-center">
-                <button class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Añadir al menu</button>
-             </div>
+                <button
+                  class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                >
+                  Añadir al menu
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -37,7 +47,9 @@ export default {
   methods: {
     //this.loading = true,
     getMenus() {
-      fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=&app_id=f0013073&app_key=4bdc9bf20b19635d616be8f8e39261a5&diet=balanced&cuisineType=Mediterranean&imageSize=SMALL`)
+      fetch(
+        `https://api.edamam.com/api/recipes/v2?type=public&q=&app_id=f0013073&app_key=4bdc9bf20b19635d616be8f8e39261a5&diet=balanced&cuisineType=Mediterranean&imageSize=SMALL`
+      )
         .then((response) => response.json())
         .then((data) => {
           this.data = data;
