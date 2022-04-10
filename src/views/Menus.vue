51ff6d331af7399f2fe5c11051a2a6acb3d6b3ff
tereh <terehdz.th@gmail.com>
2022-04-10 16:57:18 +0200

    carts para recetas

--- src/views/Menus.vue
+++ src/views/Menus.vue
@@ -1,38 +1,19 @@
 <template>
   <div class="lg:mx-28">
-    <div
-      class="grid mg:grid-cols-3 lg:grid-cols-8 lg:bg-blue-50 lg:gap-16 lg:p-10"
-    >
-      <div>Menus</div>
-
+    <div>
       <div v-if="loading">Loading....</div>
-
       <div v-if="data">
-        <h1>Desayunos</h1>
-        <div v-for="menu in data" :key="menu">
-          {{ data.hits[0].recipe.calories }}
+        <div class="grid grid-cols-8 bg-blue-50 text-gray-600">
+        <div v-for="menu in data" :key="menu" class="col-span-2 m-5" >
+            <div class="bg-white shadow-md p-8 rounded-xl text-center hover:shadow-2xl py-9">
+              <h1 class="mb-5 font-bold text-lg">{{ data.hits[0].recipe.label }}</h1>
+              <img class="m-auto mb-5" src="/data.hits[0].recipe.healtLabel.image/" alt="">
+              <h1 class="mb-5 font-semibold">Calorias: {{ data.hits[0].recipe.calories }}</h1>
+              <button class="border rounded-md py-2 px-4 bg-blue-50 font-semibold hover:text-white">Seleccionar</button>
+            </div>
+         </div>
         </div>
       </div>
-
-      <!-- <div class="col-span-2 bg-white shadow-md p-8 rounded-xl text-center hover:shadow-2xl py-9">
-                <div class="grid justify-center">
-                    <img src="/images/comida-sana.jpg" alt="">
-                </div>
-                <div>
-                    <h1 class="font-semibold mb-3">Calculated Weather</h1>
-                    <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, quam!</p>
-                 </div>
-            </div>
-
-             <div class="col-span-2 bg-white shadow-md p-8 rounded-xl text-center hover:shadow-2xl py-9">
-                <h1 class="mb-5 font-semibold">Registra tu actividad!</h1>
-                <img class="m-auto mb-2" src="/images/actividad-fisica.jpg" alt="">
-            </div>
-             <div class="col-span-2 bg-white shadow-md p-8 rounded-xl text-center hover:shadow-2xl py-9">
-                <h1 class="mb-5 font-semibold">Registra tu actividad!</h1>
-                <img class="m-auto mb-2" src="/images/actividad-fisica.jpg" alt="">
-            </div>
-             -->
     </div>
   </div>
 </template>
@@ -53,9 +34,7 @@ export default {
   methods: {
     //this.loading = true,
     getMenus() {
-      fetch(`https://api.edamam.com/search?
-            &app_id=${this.appId}&app_key=${this.appKey}
-            &q=pizza`)
+      fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=&app_id=f0013073&app_key=4bdc9bf20b19635d616be8f8e39261a5&diet=balanced&cuisineType=Mediterranean&imageSize=SMALL`)
         .then((response) => response.json())
         .then((data) => {
           this.data = data;
