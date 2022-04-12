f72cc83924061876512f9733ecdecadb5ce09bf1
Jennifer Semanate <jennifersemanate@MacBook-Air-de-Jennifer.local>
2022-04-12 15:36:37 +0200

    form

--- src/views/Notas.vue
+++ src/views/Notas.vue
@@ -1,29 +1,46 @@
 <template>
-  <div class="grid grid-cols-8 gap-5 lg:mx-28 lg:bg-[url('/images/fondo-metalizado.jpeg')]">
+  <div
+    class="grid grid-cols-8 gap-5 lg:mx-28 lg:bg-[url('/images/fondo-metalizado.jpeg')]"
+  >
     <div class="col-span-8 flex justify-between mx-5">
-      <RouterLink :to="`/`"><i class="fa-solid fa-angle-left text-2xl text-black m-5"></i></RouterLink>
+      <RouterLink :to="`/`"
+        ><i class="fa-solid fa-angle-left text-2xl text-black m-5"></i
+      ></RouterLink>
       <RouterLink :to="`/notas/${$route.params.day}/menus`"
-      ><i class="fa-solid fa-angle-right text-2xl text-black"></i></RouterLink>
+        ><i class="fa-solid fa-angle-right text-2xl text-black"></i
+      ></RouterLink>
     </div>
-    <div class="col-span-4 bg-white/60  shadow-md p-8 rounded-xl mx-16">
+    <div class="col-span-4 bg-white/60 shadow-md p-8 rounded-xl mx-16">
       <h1 class="mb-5 font-semibold">Registra tu actividad!</h1>
       <img class="m-auto mb-2 rounded-3xl" src="/images/metrics.jpeg" alt="" />
       <!-- añadir peso, altura y calorias quemadas -->
       <form class="border p-5" @submit="storeDayInfo">
         <p class="mb-2 uppercase">medidas y actividad fisica!</p>
-        
-        
+
         <div class="flex mb-4">
-          <label class="mr-2"><i class="fa-solid fa-cake-candles"></i></label>
-          <input placeholder=" Edad: 30"
-          class="border"
-          type="text"
-          v-model="edad"/>
-          <label class="mr-2"><i class="fa-solid fa-weight-scale mr-3"></i></label>
-          <input placeholder=" Peso: 65" class="border" type="text" v-model="peso"/>
-          <p class="ml-2">kg</p>
+          <label class="mr-2"
+            ><i class="fa-solid fa-cake-candles mr-3"></i
+          ></label>
+          <input
+            placeholder=" Edad: 30"
+            class="border"
+            type="text"
+            v-model="edad"
+          />
         </div>
 
+        <div class="flex mb-4">
+          <label class="mr-2"
+            ><i class="fa-solid fa-weight-scale mr-3"></i
+          ></label>
+          <input
+            placeholder=" Peso: 65"
+            class="border"
+            type="text"
+            v-model="peso"
+          />
+          <p class="ml-2">kg</p>
+        </div>
 
         <div class="flex mb-4">
           <label class="mr-2"
@@ -38,7 +55,8 @@
           <p class="ml-2">cm</p>
         </div>
         <div class="flex mb-5">
-          <label class="mr-2"><i class="fa-solid fa-fire-flame-curved mr-4"></i
+          <label class="mr-2"
+            ><i class="fa-solid fa-fire-flame-curved mr-4"></i
           ></label>
           <input
             placeholder=" Actividad: 300"
@@ -49,7 +67,9 @@
           <p class="ml-2">kcal</p>
         </div>
         <div class="flex justify-center">
-          <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-44 rounded-full">
+          <button
+            class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-44 rounded-full"
+          >
             Registrar
           </button>
         </div>
@@ -58,11 +78,7 @@
     <!-- Añadir menú -->
     <div class="col-span-4 shadow-md p-8 bg-white/60 rounded-xl mx-16">
       <h1 class="mb-5 font-semibold">Elige tu menú!</h1>
-      <img
-        class="m-auto mb-2 rounded-3xl"
-        src="/images/food.jpeg"
-        alt=""
-      />
+      <img class="m-auto mb-2 rounded-3xl" src="/images/food.jpeg" alt="" />
       <!-- añadir peso, altura y calorias quemadas -->
       <div class="grid border p-5">
         <p class="pb-8">RECETAS FITNESS!</p>
@@ -74,26 +90,27 @@
         </p>
         <div class="flex justify-center">
           <RouterLink
-          :to="`/notas/${$route.params.day}/menus`"
-          class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-28 rounded-full"
-          >Añadir Alimentacion</RouterLink>
+            :to="`/notas/${$route.params.day}/menus`"
+            class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-28 rounded-full"
+            >Añadir Alimentacion</RouterLink
+          >
         </div>
       </div>
     </div>
     <!-- notas diarias  -->
-    <div class="col-span-8 bg-white/70  shadow-md rounded p-8 m-16">
+    <div class="col-span-8 bg-white/70 shadow-md rounded p-8 m-16">
       <h1 class="mb-5 font-semibold">Notas diarias!</h1>
       <div>
-          <p>IMB:{{  calculoImb }}</p>
-          <p>Total Calorias Diarias: {{ totalCaloriasDiarias }}</p>
+        <p>IMB:{{ calculoImb }}</p>
+        <p>Total Calorias Diarias: {{ totalCaloriasDiarias }}</p>
       </div>
       <div class="grid border p-5 justify-center">
         <div class="border-b-2">
-            <p> Edad:{{ notas.edad }}</p>
-            <p>Peso:{{notas.peso}}</p>
-            <p> Altura:{{notas.altura}}</p>
-            <p>Calorias Quemadas:{{notas.quemadas}}</p>
-            <p>Calorias Consumidas:{{notas.consumidas}}</p>
+          <p>Edad:{{ notas.edad }}</p>
+          <p>Peso:{{ notas.peso }}</p>
+          <p>Altura:{{ notas.altura }}</p>
+          <p>Calorias Quemadas:{{ notas.quemadas }}</p>
+          <p>Calorias Consumidas:{{ notas.consumidas }}</p>
         </div>
       </div>
     </div>
@@ -111,12 +128,11 @@ export default {
       month: new Date().getMonth(),
       notas: {},
       quemadas: "",
-      edad:"",
+      edad: "",
       peso: "",
       altura: "",
-      imb:"",
+      imb: "",
       consumidas: "",
-      
     };
   },
   computed: {
@@ -130,12 +146,12 @@ export default {
       return days;
     },
     calculoImb() {
-        const imb = (this.peso * 10) + (this.altura * 6.25) - (this.edad * 5) -(161)
-        return imb
+      const imb = this.peso * 10 + this.altura * 6.25 - this.edad * 5 - 161;
+      return imb;
     },
     totalCaloriasDiarias() {
-        const kcal = (this.consumidas - this.quemadas) //traer las quemadas
-        return kcal
+      const kcal = this.consumidas - this.quemadas; //traer las quemadas
+      return kcal;
     },
   },
   methods: {
